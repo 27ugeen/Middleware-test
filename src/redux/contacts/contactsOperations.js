6396822a4bbd350c10f94fef962a3eee1bc2ba91
27ugeen <27ugeen@gmail.com>
2020-03-21 21:08:27 +0200

    add axios

--- src/redux/contacts/contactsOperations.js
+++ src/redux/contacts/contactsOperations.js
@@ -1,80 +1,39 @@
-// import axios from 'axios';
+import axios from 'axios';
 import contactsActions from './contactsActions';
 
-// axios.defaults.baseURL = 'http://localhost:9000';
+const BASE_URL = 'https://goit-phonebook-api.herokuapp.com/v1';
+const AUTH_TOKEN = '977f4503-57e7-4399-a1b5-3ea624a7599a';
 
-const baseUrl = 'https://goit-phonebook-api.herokuapp.com/v1';
-const authToken = '977f4503-57e7-4399-a1b5-3ea624a7599a';
+axios.defaults.baseURL = BASE_URL;
+axios.defaults.headers.common['Authorization'] = AUTH_TOKEN;
+axios.defaults.headers.post['Content-Type'] = 'application/json';
 
 const addContact = (name, number) => dispatch => {
   dispatch(contactsActions.addContactRequest());
 
-  fetch(`${baseUrl}/contacts`, {
-    method: 'POST',
-    headers: {
-      Authorization: `${authToken}`,
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({ name, number }),
-  })
-    .then(resp => resp.json())
-    .then(data => dispatch(contactsActions.addContactSuccess(data)))
+  axios
+    .post('/contacts', { name, number })
+    .then(({ data }) => dispatch(contactsActions.addContactSuccess(data)))
     .catch(error => dispatch(contactsActions.addContactError(error)));
 };
-
-// const addContact = (name, number) => dispatch => {
-//   dispatch(contactsActions.addContactRequest());
-
-//   axios
-//     .post('/contacts', { name, number })
-//     .then(resp => dispatch(contactsActions.addContactSuccess(resp.data)))
-//     .catch(error => dispatch(contactsActions.addContactError(error)));
-// };
 
 const fetchCotacts = () => dispatch => {
   dispatch(contactsActions.fetchContactsRequest());
 
-  fetch(`${baseUrl}/contacts`, {
-    method: 'GET',
-    headers: {
-      Authorization: `${authToken}`,
-    },
-  })
-    .then(resp => resp.json())
-    .then(data => dispatch(contactsActions.fetchContactsSuccess(data)))
+  axios
+    .get('/contacts')
+    .then(({ data }) => dispatch(contactsActions.fetchContactsSuccess(data)))
     .catch(error => contactsActions.fetchContactsError(error));
 };
-
-// const fetchCotacts = () => dispatch => {
-//   dispatch(contactsActions.fetchContactsRequest());
-
-//   axios
-//     .get('/contacts')
-//     .then(({ data }) => dispatch(contactsActions.fetchContactsSuccess(data)))
-//     .catch(error => contactsActions.fetchContactsError(error));
-// };
 
 const deleteContact = id => dispatch => {
   dispatch(contactsActions.deleteContactRequest());
 
-  fetch(`${baseUrl}/contacts/${id}`, {
-    method: 'DELETE',
-    headers: {
-      Authorization: `${authToken}`,
-    },
-  })
+  axios
+    .delete(`/contacts/${id}`)
     .then(() => dispatch(contactsActions.deleteContactSuccess(id)))
     .catch(error => dispatch(contactsActions.deleteContactError(error)));
 };
-
-// const deleteContact = id => dispatch => {
-//   dispatch(contactsActions.deleteContactRequest());
-
-//   axios
-//     .delete(`/contacts/${id}`)
-//     .then(() => dispatch(contactsActions.deleteContactSuccess(id)))
-//     .catch(error => dispatch(contactsActions.deleteContactError(error)));
-// };
 
 export default {
   addContact,
